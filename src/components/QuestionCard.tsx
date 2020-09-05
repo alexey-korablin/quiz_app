bf256e8fa2f11ef6c2d41d58fa360cbce70fb838
Alexey Korablin <alex8456@bk.ru>
2020-09-05 09:29:24 +0300

    01 React/TypeScript - Quiz App

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {
+  question: string;
+  answer: string[];
+  callback: any;
+  userAnswer: string;
+  questionNumber: number;
+  totalQuestion: number;
+};
+
+const QuestionCard = () => <div>QuestionCard</div>;
+
+export default QuestionCard;
